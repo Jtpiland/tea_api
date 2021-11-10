fd413354fd0d3ddea95f57bfe80146b241c50651
Jacob Piland <piland.jacob@gmail.com>
2021-11-09 18:10:14 -0600

    Add functionality to cancel subscription endpoint

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -2,12 +2,21 @@ class Api::V1::CustomerSubscriptionsController < ApplicationController
 
   def create
     customer_subscription = CustomerSubscription.create(customer_subscription_params)
-    render status: 200
+    subscription = Subscription.where('id = ?', params[:subscription_id])
+    render json: SubscriptionSerializer.new(subscription)
   end
+
+  def update
+    customer_subscription = CustomerSubscription.where('customer_id = ? AND subscription_id = ?', params[:customer_id], params[:subscription_id])
+    customer_subscription.update(customer_subscription_params)
+
+    render json: CustomerSubscriptionSerializer.new(customer_subscription)
+  end
+
 
   private
 
   def customer_subscription_params
-    params.permit(:customer_id, :subscription_id)
+    params.permit(:customer_id, :subscription_id, :status)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :customers, only: [:create, :destroy, :show] do
-        resources :customer_subscriptions, only: [:index, :create]
+        resources :customer_subscriptions, only: [:index, :create, :update]
       end
     end
   end
